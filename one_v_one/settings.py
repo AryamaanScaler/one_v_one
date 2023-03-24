d759accc4e6e1eec2ea0e592e6f86bc869669b70
AryamaanScaler <aryamaan.jain@scaler.com>
2023-03-24 16:40:00 +0530

    debug true

--- one_v_one/settings.py
+++ one_v_one/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%z)hrd2@abyh%_3t#vvc!g08ob^257p*u9*w9e5$ix(2gkp)+h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
