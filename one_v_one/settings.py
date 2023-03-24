3c91a43c51706b0d4664f47756e2d42a32874bb7
AryamaanScaler <aryamaan.jain@scaler.com>
2023-03-24 16:33:24 +0530

    config and structure changes

--- one_v_one/settings.py
+++ one_v_one/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'schedule',
 ]
 
 MIDDLEWARE = [
@@ -73,12 +74,7 @@ WSGI_APPLICATION = 'one_v_one.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
